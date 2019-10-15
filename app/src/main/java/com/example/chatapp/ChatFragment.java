1d8bf115b78a86c62cc8f9779ce9092c511cac33
KaranParwani1116 <karanparwani.parwani102@gmail.com>
2019-10-16 00:36:31 +0530

    contacts added to chat

--- app/src/main/java/com/example/chatapp/ChatFragment.java
+++ app/src/main/java/com/example/chatapp/ChatFragment.java
@@ -1,13 +1,37 @@
 package com.example.chatapp;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class ChatFragment extends Fragment {
+    private RecyclerView privatechatview;
+    private View privatechat;
+    private FirebaseAuth mAuth;
+    private DatabaseReference Contactsref;
+    private String currentuserid;
+    private DatabaseReference UsersRef;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -22,7 +46,82 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        privatechat=inflater.inflate(R.layout.fragment_chat, container, false);
+
+        privatechatview=(RecyclerView)privatechat.findViewById(R.id.private_chat_recycler_view);
+        privatechatview.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAuth=FirebaseAuth.getInstance();
+
+        Contactsref=FirebaseDatabase.getInstance().getReference().child("Contacts");
+        UsersRef=FirebaseDatabase.getInstance().getReference().child("User");
+
+        currentuserid=mAuth.getCurrentUser().getUid();
+
+
+        return privatechat;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts>options=
+                new FirebaseRecyclerOptions.Builder<Contacts>().setQuery(Contactsref.child(currentuserid),Contacts.class).build();
+
+        FirebaseRecyclerAdapter<Contacts,ChatViewHolder> adapter=new FirebaseRecyclerAdapter<Contacts, ChatViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final ChatViewHolder chatViewHolder, int i, @NonNull Contacts contacts) {
+               final String userid=getRef(i).getKey();
+               UsersRef.child(userid).addValueEventListener(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                       if(dataSnapshot.hasChild("image"))
+                       {
+                           final String retimage=dataSnapshot.child("image").getValue().toString();
+                           Picasso.get().load(retimage).placeholder(R.drawable.profile_image).fit().into(chatViewHolder.profile_photo);
+
+                       }
+
+                       final String name=dataSnapshot.child("name").getValue().toString();
+                       final String status=dataSnapshot.child("status").getValue().toString();
+
+                       chatViewHolder.username.setText(name);
+                       chatViewHolder.status.setText("Last Seen: " + "\n" + "Date " +" Time");
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                   }
+               });
+
+            }
+
+            @NonNull
+            @Override
+            public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.users_view,parent,false);
+                ChatViewHolder holder=new ChatViewHolder(view);
+                return holder;
+            }
+        };
+
+        privatechatview.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class ChatViewHolder extends RecyclerView.ViewHolder {
+        private TextView username;
+        private CircleImageView profile_photo;
+        private TextView status;
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = (itemView).findViewById(R.id.user_name);
+            status = (itemView).findViewById(R.id.user_status);
+            profile_photo = (itemView).findViewById(R.id.users_profile_image);
+
+        }
+    }
 }
--- app/src/main/java/com/example/chatapp/RequestFragment.java
+++ app/src/main/java/com/example/chatapp/RequestFragment.java
@@ -1,12 +1,14 @@
 package com.example.chatapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,9 +18,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,7 +39,7 @@ public class RequestFragment extends Fragment {
     private RecyclerView Requests_List;
     private DatabaseReference ChatRequesstRef;
     private FirebaseAuth mAuth;
-    private DatabaseReference UserRef;
+    private DatabaseReference UserRef, Contactsref;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,7 +62,8 @@ public class RequestFragment extends Fragment {
 
         ChatRequesstRef = FirebaseDatabase.getInstance().getReference().child("Chat Requests");
         mAuth = FirebaseAuth.getInstance();
-        UserRef=FirebaseDatabase.getInstance().getReference().child("User");
+        UserRef = FirebaseDatabase.getInstance().getReference().child("User");
+        Contactsref = FirebaseDatabase.getInstance().getReference().child("Contacts");
 
         return convert;
     }
@@ -71,83 +77,155 @@ public class RequestFragment extends Fragment {
                         .setQuery(ChatRequesstRef.child(mAuth.getCurrentUser().getUid()), Contacts.class)
                         .build();
 
-        FirebaseRecyclerAdapter<Contacts,RequestViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts,RequestViewHolder>(options) {
+        FirebaseRecyclerAdapter<Contacts, RequestViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, RequestViewHolder>(options) {
             @NonNull
             @Override
             public RequestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.users_view,parent,false);
-                RequestViewHolder holder=new RequestViewHolder(view);
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_view, parent, false);
+                RequestViewHolder holder = new RequestViewHolder(view);
                 return holder;
             }
 
             @Override
             protected void onBindViewHolder(@NonNull final RequestViewHolder requestViewHolder, int i, @NonNull Contacts contacts) {
-                  requestViewHolder.Accept.setVisibility(View.VISIBLE);
-                  requestViewHolder.Cancel.setVisibility(View.VISIBLE);
+                requestViewHolder.Accept.setVisibility(View.VISIBLE);
+                requestViewHolder.Cancel.setVisibility(View.VISIBLE);
 
-                  final String list_User_Id=getRef(i).getKey();
+                final String list_User_Id = getRef(i).getKey();
 
-                  DatabaseReference getTypeRef=getRef(i).child("request_type").getRef();
-                  getTypeRef.addValueEventListener(new ValueEventListener() {
-                      @Override
-                      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                          if(dataSnapshot.exists())
-                          {
-                              String type=dataSnapshot.getValue().toString();
+                DatabaseReference getTypeRef = getRef(i).child("request_type").getRef();
+                getTypeRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            String type = dataSnapshot.getValue().toString();
 
-                              if(type.equals("received"))
-                              {
-                                  UserRef.child(list_User_Id).addValueEventListener(new ValueEventListener() {
-                                      @Override
-                                      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                          if(dataSnapshot.child("image").exists())
-                                          {
-                                              requestViewHolder.username.setText(dataSnapshot.child("name").getValue().toString());
-                                              requestViewHolder.userstatus.setText(dataSnapshot.child("status").getValue().toString());
-                                              Picasso.get().load(dataSnapshot.child("image").getValue().toString()).placeholder(R.drawable.profile_image).fit().into(requestViewHolder.userprofilephoto);
-                                          }
-                                          else
-                                          {
-                                              requestViewHolder.username.setText(dataSnapshot.child("name").getValue().toString());
-                                              requestViewHolder.userstatus.setText(dataSnapshot.child("status").getValue().toString());
-                                          }
-                                      }
+                            if (type.equals("received")) {
+                                UserRef.child(list_User_Id).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.child("image").exists()) {
+                                            requestViewHolder.username.setText(dataSnapshot.child("name").getValue().toString());
+                                            requestViewHolder.userstatus.setText("wants to connect with you");
+                                            Picasso.get().load(dataSnapshot.child("image").getValue().toString()).placeholder(R.drawable.profile_image).fit().into(requestViewHolder.userprofilephoto);
+                                        } else {
+                                            requestViewHolder.username.setText(dataSnapshot.child("name").getValue().toString());
+                                            requestViewHolder.userstatus.setText("wants to connect with you");
+                                        }
 
-                                      @Override
-                                      public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                                      }
-                                  });
-                              }
-                          }
-                      }
+                                        requestViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                CharSequence options[] = new CharSequence[]
+                                                        {
+                                                                "Accept",
+                                                                "Cancel"
+                                                        };
 
-                      @Override
-                      public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                                builder.setTitle(dataSnapshot.child("name").getValue().toString() + " Chat Request");
+                                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        if (which == 0) {
+                                                            Contactsref.child(mAuth.getCurrentUser().getUid()).child(list_User_Id).child("Contacts").setValue("Saved").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                    if (task.isSuccessful()) {
+                                                                        Contactsref.child(list_User_Id).child(mAuth.getCurrentUser().getUid()).child("Contacts").setValue("Saved").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                            @Override
+                                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                                if (task.isSuccessful()) {
+                                                                                    ChatRequesstRef.child(mAuth.getCurrentUser().getUid()).child(list_User_Id).removeValue()
+                                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                                @Override
+                                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                                    if (task.isSuccessful()) {
+                                                                                                        ChatRequesstRef.child(list_User_Id).child(mAuth.getCurrentUser().getUid())
+                                                                                                                .removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                                            @Override
+                                                                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                                                                if (task.isSuccessful()) {
+                                                                                                                    Toast.makeText(getContext(), "New Contact Saved", Toast.LENGTH_SHORT).show();
 
-                      }
-                  });
+                                                                                                                }
+                                                                                                            }
+                                                                                                        });
+                                                                                                    }
+                                                                                                }
+                                                                                            });
+                                                                                }
+                                                                            }
+                                                                        });
+                                                                    }
+                                                                }
+                                                            });
+
+                                                        }
+                                                        if (which == 1) {
+                                                            ChatRequesstRef.child(mAuth.getCurrentUser().getUid()).child(list_User_Id).removeValue()
+                                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                        @Override
+                                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                                            if (task.isSuccessful()) {
+                                                                                ChatRequesstRef.child(list_User_Id).child(mAuth.getCurrentUser().getUid())
+                                                                                        .removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                    @Override
+                                                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                                                        if (task.isSuccessful()) {
+                                                                                            Toast.makeText(getContext(), "New Contact Saved", Toast.LENGTH_SHORT).show();
+
+                                                                                        }
+                                                                                    }
+                                                                                });
+                                                                            }
+                                                                        }
+                                                                    });
+                                                        }
+                                                    }
+                                                });
+                                                builder.show();
+                                            }
+                                        });
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         };
         Requests_List.setAdapter(adapter);
         adapter.startListening();
     }
 
-    public static class RequestViewHolder extends RecyclerView.ViewHolder{
+    public static class RequestViewHolder extends RecyclerView.ViewHolder {
 
-        TextView username,userstatus;
+        TextView username, userstatus;
         CircleImageView userprofilephoto;
-        Button Accept,Cancel;
+        Button Accept, Cancel;
         View ItemView;
+
         public RequestViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.ItemView=itemView;
+            this.ItemView = itemView;
 
-            username=(itemView).findViewById(R.id.user_name);
-            userstatus=(itemView).findViewById(R.id.user_status);
-            userprofilephoto=(itemView).findViewById(R.id.users_profile_image);
-            Accept=(itemView).findViewById(R.id.request_accept_btn);
-            Cancel=(itemView).findViewById(R.id.request_cancel_btn);
+            username = (itemView).findViewById(R.id.user_name);
+            userstatus = (itemView).findViewById(R.id.user_status);
+            userprofilephoto = (itemView).findViewById(R.id.users_profile_image);
+            Accept = (itemView).findViewById(R.id.request_accept_btn);
+            Cancel = (itemView).findViewById(R.id.request_cancel_btn);
         }
     }
 }
